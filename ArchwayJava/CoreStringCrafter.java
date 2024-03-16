9af8833beca93a787528c6802c4574ea47ec008a
Sreelakshmi2021 <Sreelakshmi.tl@viacomcontractor.com>
2024-03-16 23:42:51 +0530

    sdnasldjaspdosadsa lease enter the commit message for your changes. Lines starting

---
+++ ArchwayJava/CoreStringCrafter.java
@@ -0,0 +1,13 @@
+public class CoreStringCrafter {
+    public static void main (String [] args) {
+        String s = "hello"; //String literal
+        String sr = new String("hello"); // Creat object for string with string class
+
+        /* both are correct, but when declare like later, it creates a new string. but no problem for both */
+
+
+
+
+
+    }
+}
