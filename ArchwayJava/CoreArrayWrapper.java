241c5ce32fb39531cd4c3370fbbddbcf9e1e99ca
Sreelakshmi2021 <Sreelakshmi.tl@viacomcontractor.com>
2024-03-16 18:16:47 +0530

    dhasd
    wqsdsfdsf

--- ArchwayJava/CoreArrayWrapper.java
+++ ArchwayJava/CoreArrayWrapper.java
@@ -23,7 +23,7 @@ public class CoreArrayWrapper {
         System.out.println("");
         System.out.println("Array length " + arr.length);
 
-        int[] brr = new int[]{23,44,55,67,78,4};
+        int[] brr = new int[]{23,44,55,67,3,78,4};
         //Declare array and initialise all  together
 
         String [] crr = new String[] {"seena", "meena", "reenu", "kalyani"};
@@ -32,44 +32,106 @@ public class CoreArrayWrapper {
         }
         System.out.println("");
 
-        //2 D array / matrix
-        /*  ________________
-            | 2  | 7  | 3  |
-            | 5  | 8  | 2  |
-            | 6  | 9  | 4  |
-            ----------------
-         */
-
-        int [][] intMatrix = new int[3][3];
-        intMatrix [0][0] = 2;
-        intMatrix [0][1] = 7;
-        intMatrix [0][2] = 3;
-        intMatrix [1][0] = 5;
-        intMatrix [1][1] = 8;
-        intMatrix [1][2] = 2;
-        intMatrix [2][0] = 6;
-        intMatrix [2][1] = 9;
-        intMatrix [2][2] = 4;
-
-        //how to print elements in a matrix ?
-        for (int i=0; i<3; i++) {
-            for (int j=0; j<3; j++) {
-                int element = intMatrix[i][j];
-                System.out.print(element + " ");
+        //Print smallest number in the array brr
+        int min = brr[0];
+        for (int num : brr) {
+            if (num < min) {
+                min = num;
             }
-            System.out.println("");
+        }
+        System.out.println("minimum value in the array : " + min);
+
+        //Find sum of elements in an array drr
+        int[] drr = new int[] {10,20,30,40,50};
+        int sum = 0;
+        for (int num : drr) {
+            sum = sum + num;
+        }
+        System.out.println(sum);
+        System.out.println(sum/drr.length);
+
+        //Check if elements in array is sorted (Ascending or descending)
+        int[] err = new int[] {100, 20, 10, 3, 4};
+        boolean isAscending=false;
+        for (int i=0; i<err.length-1; i++) { // check if array is ascending
+            if (err[i] <= err[i+1]) {
+                isAscending = true;
+            } else {
+                isAscending = false;
+                break;
+            }
+        }
+        boolean isDescending=false;
+        for (int i=0; i<err.length-1; i++) { // check if array is descending
+            if (err[i] >= err[i+1]) {
+                isDescending = true;
+            } else {
+                isDescending = false;
+                break;
+            }
+        }
+        boolean isSorted = (isDescending || isAscending);
+        System.out.println("is Array sorted ? " + isSorted);
+
+        //Remove duplicates from an array
+        int[] dupArray = new int[] {2, 3, 4, 5, 3, 3, 4, 5, 7, 8};
+        int oldCount = dupArray.length;
+        int newCount = oldCount;
+        for (int i=0; i<oldCount; i++) {
+            for(int j=i+1; j<oldCount; j++) {
+                if (dupArray[i] == dupArray[j]) {
+                    newCount--;
+                    break;
+                }
+            }
+        }
+        System.out.println("newCount " +newCount);
+        int [] tempArray = new int[newCount];
+        int index=0;
+        for (int k=0; k<oldCount; k++) {
+            boolean isDuplicate = false;
+            for (int l=k+1; l<oldCount; l++) {
+                if (dupArray[k] == dupArray[l]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                tempArray[index] = dupArray[k];
+                index++;
+            }
+        }
+        dupArray = tempArray;
+        for (int ele: dupArray) {
+            System.out.print(ele + " ");
         }
 
-        //or we can initialise while declaring like this
-        int [][] integerMatrix = new int[][] {{2,7,3}, {5,8,2}, {6,9,4}};
+        // Rotate the elements of an array to the right by 2 positions.
+        String[] sree = new String[]{"minni", "cheechu", "sethu", "shalu", "kathu", "kallu"};
+        String[] tempNames = new String[sree.length];
+        int shift = 2;
+        for (int i=0; i<sree.length; i++) {
+            if (i < shift) {
+                tempNames[i] = sree[i + sree.length - shift];
+            } else {
+                tempNames[i] = sree[i - shift];
+            }
+        }
 
-        String [][] srr = new String[2][3];
-        srr[0][0] = "apple";
-        srr[0][1] = "orangle";
-        srr[0][2] = "plum";
-        srr[1][0] = "red";
-        srr[1][1] = "orangle";
-        srr[1][2] = "pink";
+        System.out.println("");
 
+        for (String names : tempNames) {
+            System.out.print(names + " ");
+        }
+        System.out.println("");
+
+        // Find the longest and shortest strings in an array.
+        int maxLength = 0;
+        for (String s: sree) {
+            if (s.length() > maxLength) {
+                maxLength = s.length();
+            }
+        }
+        System.out.println("max length " + maxLength);
     }
 }
---
+++ ArchwayJava/CoreMultiDimArrayWrapper.java
@@ -0,0 +1,73 @@
+public class CoreMultiDimArrayWrapper {
+    public static void main (String [] args) {
+        //2 D array / matrix
+        /*  ________________
+            | 2  | 7  | 3  |
+            | 5  | 8  | 2  |
+            | 6  | 9  | 4  |
+            ----------------
+         */
+
+        int [][] intMatrix = new int[3][3];
+        intMatrix [0][0] = 2;
+        intMatrix [0][1] = 7;
+        intMatrix [0][2] = 3;
+        intMatrix [1][0] = 5;
+        intMatrix [1][1] = 8;
+        intMatrix [1][2] = 2;
+        intMatrix [2][0] = 6;
+        intMatrix [2][1] = 9;
+        intMatrix [2][2] = 4;
+
+        //how to print elements in a matrix ?
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                int element = intMatrix[i][j];
+                System.out.print(element + " ");
+            }
+            System.out.println("");
+        }
+
+        //or we can initialise while declaring like this
+        int [][] integerMatrix = new int[][] {{2,7,3}, {5,8,2}, {6,9,4}};
+
+        //how to find the number of rows and coloums of a matrix
+        int rows = integerMatrix.length;
+        int cols = integerMatrix[0].length; //take first row and get it's length
+
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                int element = intMatrix[i][j];
+                System.out.print(element + " ");
+            }
+            System.out.println("");
+        }
+
+        //This is how string multi dimensional arrays looks like !!
+        String [][] srr = new String[2][3];
+        srr[0][0] = "apple";
+        srr[0][1] = "orange";
+        srr[0][2] = "plum";
+        srr[1][0] = "red";
+        srr[1][1] = "orange";
+        srr[1][2] = "pink";
+
+        for (String[] rowValues : srr) {
+            for (String strings : rowValues) {
+                System.out.println(strings);
+            }
+        }
+
+        //print smallest number in a matrix. let's take integerMatrix
+        int min = integerMatrix[0][0];
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                int num = integerMatrix[i][j];
+                if (num < min) {
+                    min = num;
+                }
+            }
+        }
+        System.out.println("Minimum value from the matrix : " + min);
+    }
+}
