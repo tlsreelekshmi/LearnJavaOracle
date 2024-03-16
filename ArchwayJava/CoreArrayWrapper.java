8452c44f65c072ace1d95ef31778fc673950501b
Sreelakshmi2021 <Sreelakshmi.tl@viacomcontractor.com>
2024-03-16 18:30:54 +0530

    commit mehod find element in an array string

--- ArchwayJava/CoreArrayWrapper.java
+++ ArchwayJava/CoreArrayWrapper.java
@@ -1,6 +1,6 @@
 public class CoreArrayWrapper {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         int arr[] = new int[5];
         arr[0] = 3;
         arr[1] = 5;
@@ -23,10 +23,10 @@ public class CoreArrayWrapper {
         System.out.println("");
         System.out.println("Array length " + arr.length);
 
-        int[] brr = new int[]{23,44,55,67,3,78,4};
+        int[] brr = new int[]{23, 44, 55, 67, 3, 78, 4};
         //Declare array and initialise all  together
 
-        String [] crr = new String[] {"seena", "meena", "reenu", "kalyani"};
+        String[] crr = new String[]{"seena", "meena", "reenu", "kalyani"};
         for (String s : crr) {
             System.out.print(s + " ");
         }
@@ -42,28 +42,28 @@ public class CoreArrayWrapper {
         System.out.println("minimum value in the array : " + min);
 
         //Find sum of elements in an array drr
-        int[] drr = new int[] {10,20,30,40,50};
+        int[] drr = new int[]{10, 20, 30, 40, 50};
         int sum = 0;
         for (int num : drr) {
             sum = sum + num;
         }
         System.out.println(sum);
-        System.out.println(sum/drr.length);
+        System.out.println(sum / drr.length);
 
         //Check if elements in array is sorted (Ascending or descending)
-        int[] err = new int[] {100, 20, 10, 3, 4};
-        boolean isAscending=false;
-        for (int i=0; i<err.length-1; i++) { // check if array is ascending
-            if (err[i] <= err[i+1]) {
+        int[] err = new int[]{100, 20, 10, 3, 4};
+        boolean isAscending = false;
+        for (int i = 0; i < err.length - 1; i++) { // check if array is ascending
+            if (err[i] <= err[i + 1]) {
                 isAscending = true;
             } else {
                 isAscending = false;
                 break;
             }
         }
-        boolean isDescending=false;
-        for (int i=0; i<err.length-1; i++) { // check if array is descending
-            if (err[i] >= err[i+1]) {
+        boolean isDescending = false;
+        for (int i = 0; i < err.length - 1; i++) { // check if array is descending
+            if (err[i] >= err[i + 1]) {
                 isDescending = true;
             } else {
                 isDescending = false;
@@ -74,23 +74,23 @@ public class CoreArrayWrapper {
         System.out.println("is Array sorted ? " + isSorted);
 
         //Remove duplicates from an array
-        int[] dupArray = new int[] {2, 3, 4, 5, 3, 3, 4, 5, 7, 8};
+        int[] dupArray = new int[]{2, 3, 4, 5, 3, 3, 4, 5, 7, 8};
         int oldCount = dupArray.length;
         int newCount = oldCount;
-        for (int i=0; i<oldCount; i++) {
-            for(int j=i+1; j<oldCount; j++) {
+        for (int i = 0; i < oldCount; i++) {
+            for (int j = i + 1; j < oldCount; j++) {
                 if (dupArray[i] == dupArray[j]) {
                     newCount--;
                     break;
                 }
             }
         }
-        System.out.println("newCount " +newCount);
-        int [] tempArray = new int[newCount];
-        int index=0;
-        for (int k=0; k<oldCount; k++) {
+        System.out.println("newCount " + newCount);
+        int[] tempArray = new int[newCount];
+        int index = 0;
+        for (int k = 0; k < oldCount; k++) {
             boolean isDuplicate = false;
-            for (int l=k+1; l<oldCount; l++) {
+            for (int l = k + 1; l < oldCount; l++) {
                 if (dupArray[k] == dupArray[l]) {
                     isDuplicate = true;
                     break;
@@ -102,7 +102,7 @@ public class CoreArrayWrapper {
             }
         }
         dupArray = tempArray;
-        for (int ele: dupArray) {
+        for (int ele : dupArray) {
             System.out.print(ele + " ");
         }
 
@@ -110,7 +110,7 @@ public class CoreArrayWrapper {
         String[] sree = new String[]{"minni", "cheechu", "sethu", "shalu", "kathu", "kallu"};
         String[] tempNames = new String[sree.length];
         int shift = 2;
-        for (int i=0; i<sree.length; i++) {
+        for (int i = 0; i < sree.length; i++) {
             if (i < shift) {
                 tempNames[i] = sree[i + sree.length - shift];
             } else {
@@ -127,11 +127,21 @@ public class CoreArrayWrapper {
 
         // Find the longest and shortest strings in an array.
         int maxLength = 0;
-        for (String s: sree) {
+        for (String s : sree) {
             if (s.length() > maxLength) {
                 maxLength = s.length();
             }
         }
         System.out.println("max length " + maxLength);
+
+        //Check if a given string exists in the array. - kathu
+        for(String name :sree) {
+            if (name =="kathu") {
+                System.out.println("String \'kathu\' exists in array sree");
+                break;
+            } else {
+                System.out.printf("name : %s String 'kathu' does not found%n", name);
+            }
+        }
     }
 }
